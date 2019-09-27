e4cc7faf49c4f4c06c87067e24ee3b729bd8991d
ismynr <gekkai7is.my@gmail.com>
2019-09-28 02:11:41 +0700

    tugas

---
+++ ismi-nururrizqi_17090042_4D.java
@@ -0,0 +1,7 @@
+package tugas;
+
+public class datang {
+    public static void main(String[] args){
+        system.out.println("Selamat datang di Object Oriented Programming \n isminururrizqi_17090042_4D");
+    }
+}
